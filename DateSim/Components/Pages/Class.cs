77c42f6734b37b539db74b3bf8642bda3a83414d
Alalaizhon <katagawa.k@gmail.com>
2024-12-21 08:07:04 +0500

    Add project files.

---
+++ DateSim/Components/Pages/Class.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class AppState
+{
+	public List<Profile> LikedProfiles { get; set; } = new List<Profile>();
+}
+
+public class Profile
+{
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public List<string> Interests { get; set; }
+	public string ImageUrl { get; set; }
+}
